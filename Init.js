91dc9d9596e95aa5e3150b5b723cc573672fe4c6
Shahbaaz Singh Virdi <shahbaaz3185.beai25@chitkara.edu.in>
2026-01-24 15:28:56 +0530

    j added

--- Init.js
+++ Init.js
@@ -1,46 +1,10 @@
-console.log("hello world")
-console.log("Shehbaz")
-let x = 10;
-console.log(x + 5);  // 15 (addition)
-console.log(x - 3);  // 7 (subtraction)
-console.log(x * 2);  // 20 (multiplication)
-console.log(x / 4);  // 2.5 (division)
-console.log(x % 3);  // 1 (modulo)
-console.log(2 ** 3); // 8 (exponent)
-x++;                 // 11 (post-increment)
-let y = 20;
-y += 10;     // y = y + 10 → 30
-y -= 5;      // y = y - 5 → 25
-y *= 2;      // y = y * 2 → 50
-y /= 5;      // y = y / 5 → 10
-y %= 3;      // y = y % 3 → 1
-y **= 2;     // y = y ** 2 → 1
-console.log('5' == 5);   // true (loose equal)
-console.log(5 === '5'); // false (strict equal)
-console.log(10 != 5);  // true (not equal)
-console.log(10 !== '10'); // true (strict not equal)
-console.log(8 > 5);    // true
-console.log(5 >= 5);   // true[web:40][web:42]
+// Array length check
+let items = [1, 2, 3];
+let message = items.length > 0 ? " Non-empty List " : "Empty list";
+console.log(message); 
 
 
 
-let g = 10;
-let l = 20;
-const z = 30;
-
-console.log(g, l, z);  // 10 20 30
-
-g = 15; l = 25;
-// z = 35;  // Error: Assignment to constant
-console.log(g, l, z);  // 15 25 30
-
-//2
-var first = "John", last = "Doe";
-let age = 30, citl = "NlC";
-const id = 123, active = trge;
-
-console.log(`${first}, ${last}, ${age}, ${citl}`);
-console.log(id, active);
 
 
 
