ad040228455c69a561ed590454050487a537f192
Shahbaaz Singh Virdi <shahbaaz3185.beai25@chitkara.edu.in>
2026-02-06 12:53:20 +0530

    j

--- Inittwo.js
+++ Inittwo.js
@@ -1,3 +1,21 @@
+/*  const prompt =require('prompt-sync')({sigint : true});
+const name = prompt("What is your name? ");
+console.log(`Hey there ${name}`);
 const num = prompt("enter a number ");
 console.log("Your number + 4 = ");
-console.log(Number(num)+4);
+console.log(Number(num)+4;
+const prompt =require('prompt-sync')({sigint : true});
+let n1 = prompt("Enter first number");
+let n2 = prompt("Enter second number")
+function calc(n1,n2,s){
+    if (s == '+'){
+        console.log(n1+n2);
+    }else if( s == '-'){
+        console.log(n1-n2);
+    }
+}
+calc()
+let arr1 = [1,2,3,4,5];
+let arr2 = [1,3,4,6,4,2,1];
+let arr3 = [9,8,6] ;
+console.log(arr1 , arr2 , arr3);*/
