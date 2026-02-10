45227c1470e5b6ccbbcc54b5b2ecfdeb280ebf43
Shahbaaz Singh Virdi <shahbaaz3185.beai25@chitkara.edu.in>
2026-02-10 10:40:49 +0530

    l

--- Inittwo.js
+++ Inittwo.js
@@ -21,6 +21,7 @@ let arr3 = [9,8,6] ;
 console.log(arr1 , arr2 , arr3);*/
 //
 // 
+const prompt =require('prompt-sync')({sigint : true});
 let n = parseInt(prompt("Enter the size of the square (number):"));
 
 if (n < 1) {
@@ -50,4 +51,16 @@ console.log(obj);
 let obj2={...obj,"age":277};
 for(const key in obj2){
     let element = obj2[key];
+    }
+let marks ={
+    h:90,
+    s:40,
+    shi:56,
+    sh:90,
+}
+let obj3 ={...marks,"h":100}
+console.log(marks)
+console.log(obj2)
+function sum(...name1){
+    console.log(name1);
 }
