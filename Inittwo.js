767975f4f1a5dcc6e27afcdc11d47ca8afc635f4
Shahbaaz Singh Virdi <shahbaaz3185.beai25@chitkara.edu.in>
2026-03-01 21:03:35 +0530

    u

--- Inittwo.js
+++ Inittwo.js
@@ -103,6 +103,7 @@ const multiplied = nums.map((n, i) => n * i);
 console.log(multiplied); // [0, 6, 14] 
 // 
 */
+/*
 const prompt = require('prompt-sync')();
 let name = prompt("Enter your name:");
 let age = Number(prompt("Enter your age:"));
@@ -554,4 +555,40 @@ console.log(f29);
 let a30 = [0, 1, ""];
 for (let i = 0; i < 3; i++) if (a30[i]) console.log("T: " + a30[i]);
 // Input: None
-// Output: T: 1
+// Output: T: 1 */
+/*
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App*/
